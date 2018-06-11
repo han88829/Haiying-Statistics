85d1b780f7a5f0487a46cad458107dbbe9acc72c
Han <h88829@foxmail.com>
2018-06-11 21:01:47 +0800

    已修改        build/webpack.base.conf.js 已修改        src/pages/index/index.vue 已修改        src/pages/index/main.js

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,36 +1,25 @@
 <template>
-  <div class="container" @click="clickHandle('test click', $event)">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
+<div class="root">
+  <div class="content"> 
+      这是主页FFFF
   </div>
+  <div class="footer" > 
+     <div v-bind:class="{select:type}" v-on:click="onChangetype(true)">列表</div>
+     <div v-bind:class="{select:!type}" v-on:click="onChangetype(false)">分类</div>
+  </div>
+</div>
 </template>
 
 <script>
-import card from '@/components/card'
+import card from "@/components/card";
 
 export default {
-  data () {
+  data() {
     return {
-      motto: 'Hello World',
-      userInfo: {}
-    }
+      motto: "Hello World",
+      userInfo: {},
+      type: true
+    };
   },
 
   components: {
@@ -38,68 +27,68 @@ export default {
   },
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      wx.navigateTo({ url })
+    bindViewTap() {
+      const url = "../logs/main";
+      wx.navigateTo({ url });
     },
-    getUserInfo () {
+    getUserInfo() {
       // 调用登录接口
       wx.login({
         success: () => {
           wx.getUserInfo({
-            success: (res) => {
-              this.userInfo = res.userInfo
+            success: res => {
+              this.userInfo = res.userInfo;
             }
-          })
+          });
         }
-      })
+      });
     },
-    clickHandle (msg, ev) {
-      console.log('clickHandle:', msg, ev)
+    onChangetype(type) {
+      this.type = type;
     }
   },
 
-  created () {
+  created() {
     // 调用应用实例的方法获取全局数据
-    this.getUserInfo()
+    this.getUserInfo();
   }
-}
+};
 </script>
 
 <style scoped>
-.userinfo {
+.root {
+  height: 100vh;
+  width: 100vw;
+  background: #fff;
   display: flex;
+  display: -webkit-flex;
   flex-direction: column;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+.content {
+  flex: 1;
+  width: 100vw;
+  background: yellow;
+}
+.footer {
+  height: 45px;
+  width: 100vw;
+  background: #fff;
+  display: flex;
+  display: -webkit-flex;
+  flex-direction: row;
   align-items: center;
 }
-
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
+.footer > div {
+  flex: 1;
+  height: 45px;
+  line-height: 45px;
+  text-align: center;
 }
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-
-.counter {
-  display: inline-block;
-  margin: 10px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
+.select {
+  color: #108ee9;
 }
 </style>
